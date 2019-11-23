22c123d6738a8db2900e43ce26cefc18c89ac312
Sai Khae Leang <sai@Sais-MacBook-Pro.local>
2019-11-23 18:11:54 +0630

    react-router-dom 1

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+import axios from '../../axios';
 
 class Blog extends Component {
     state = {
@@ -24,7 +24,7 @@ class Blog extends Component {
             })
             .catch(error => {
                 this.setState({ error: true });
-                console.log(error);
+                console.log("Blog Error : " + error);
             });
 
     }
@@ -46,15 +46,17 @@ class Blog extends Component {
                 />;
             })
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">HOME</a></li>
+                            <li><a href="/new-post">NEW POST</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorisation"] = "Auth Yes";
+axios.defaults.headers.post['Content-type'] = "application/json";
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+},
+    error => {
+        console.log("Interceptors error : " + error);
+        return Promise.reject(error);
+    });
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log("Interceptors Error [response] : " + error);
+    return Promise.reject(error);
+});
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
